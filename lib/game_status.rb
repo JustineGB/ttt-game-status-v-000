a4ed02f1533f2583a54e21b2609a4da906c97441
Justine Bricknell <justinegayton@gmail.com>
2018-06-12 14:54:13 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -55,17 +55,15 @@ end
 
 def over?(board)
   #call on draw?, full?, and won? All will return FALSE unless the game is not yet over
-  if full?(board) || !(won?(board)) || draw?(board) 
+  if full?(board) || !(won?(board)) || draw?(board)
     return true
   else
     return false
   end
-end 
-
- def winner(board)
-   #if X or O won..WIN_COMBINATIONS[]
-   if win_combination = won?(board)
-  puts "#{win_combination}"
-   elsif !won?(board)
 end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
 end

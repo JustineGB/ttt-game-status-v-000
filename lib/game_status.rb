b35fceb29589e436ebc617c6c74dcca1a040f092
Justine Bricknell <justinegayton@gmail.com>
2018-06-12 14:02:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,8 +29,8 @@ def won?(board)
 
   if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 =="O" && position_2 == "O" && position_3 == "O"
      return win_combination
-  elsif !position_taken?(board)
-    else 
+  elsif !position_taken?(board, index)
+    else
       false
   end
  end
